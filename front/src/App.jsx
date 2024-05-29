bd8236290a55cf241e3e73c3ea3b5826ded33362
NicoLopezBjj <lopeznnr@gmail.com>
2024-05-29 11:22:12 -0300

    add middlewares passport in route editbook

---
+++ front/src/App.jsx
@@ -0,0 +1,22 @@
+
+import {Header} from "./Header/Header";
+import {Hero} from "./Hero/Hero";
+import { Footer } from './Footer/footer';
+import {Navbar} from "./Navbar/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      
+      <Hero/>
+      
+      <Header/>
+
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
