df0a4b21149b6a149192517eafde6b465620b0fd
port86 <rodriguez.fernando011@gmailcom>
2024-06-07 11:37:52 -0300

    navabr subido Fer falta dar mas estilos

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,120 +1,117 @@
-import { Link } from "react-router-dom";
-import { useUserContext } from "../../hooks/useUser";
+import React, { useState } from "react";
 
 export function Navbar() {
-
-  const { user } = useUserContext()
-  console.log(user)
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <img src="https://bimdu.es/wp-content/webpc-passthru.php?src=https://bimdu.es/wp-content/uploads/2021/01/Logo-Interbooks.png&nocache=1" alt="interbooks logo" width={50} />
-          <span className="ml-3 text-xl">InterBooks</span>
-        </Link>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link to='/' className="mr-5 hover:text-gray-900">Inicio</Link>
-          <Link to='/biblioteca' className="mr-5 hover:text-gray-900">Biblioteca</Link>
-          <Link to='/wishlist' className="mr-5 hover:text-gray-900">WishList</Link>
-          <Link to='/exchanges' className="mr-5 hover:text-gray-900">Intercambios</Link>
-        </nav>
-        {
-          user 
-            ? <Link to='/account' className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Mi Cuenta</Link>
-            :
-              <Link to='/login' className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Ingresar
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </Link>
-        }
+    <nav className="w-full fixed top-0 left-0 right-0 z-10 bg-transparent">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-2"
+            >
+              <path d="M12 2L2 7l10 5 10-5L12 2z" fill="#3498db" />
+              <path d="M12 13l10-5v10l-10 5-10-5V8l10 5z" fill="#2980b9" />
+            </svg>
+            <a
+              href="#"
+              className="text-lg font-semibold"
+              style={{ color: "#33CCCC", transition: "color 0.3s" }}
+              onMouseEnter={(e) => (e.target.style.color = "grey")}
+              onMouseLeave={(e) => (e.target.style.color = "#33CCCC")}
+            >
+              Interbook
+            </a>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a
+                href="#"
+                className="text-lg font-semibold"
+                style={{ color: "#33CCCC", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "grey")}
+                onMouseLeave={(e) => (e.target.style.color = "#33CCCC")}
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-lg font-semibold"
+                style={{ color: "grey", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "#33CCCC")}
+                a
+                onMouseLeave={(e) => (e.target.style.color = "#33CCCC")}
+              >
+                Biblioteca
+              </a>
+              <a
+                href="#"
+                className="text-lg font-semibold"
+                style={{ color: "grey", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "#33CCCC")}
+                onMouseLeave={(e) => (e.target.style.color = "#33CCCC")}
+              >
+                Wishlist
+              </a>
+              <a
+                href="#"
+                className="text-lg font-semibold"
+                style={{ color: "grey", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "#33CCCC")}
+                onMouseLeave={(e) => (e.target.style.color = "#33CCCC")}
+              >
+                Intercambios
+              </a>
+            </div>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Busqueda"
+              className="bg-white-800 text-gray-300 rounded-full border-2 border-teal-400 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-teal-400"
+            />
+          </div>
+          <button
+            type="button"
+            className="ml-4 bg-teal-400 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-teal-400"
+          >
+            Iniciar sesión
+          </button>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              type="button"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
-    </header>
-    // <nav className="border-gray-200 dark:bg-transparent bg-transparent">
-    //   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    //     <a
-    //       href="https://flowbite.com/"
-    //       className="flex items-center space-x-3 rtl:space-x-reverse"
-    //     >
-    //       <img
-    //         src="file:///C:/Users/port_/Downloads/CUSTOM-group_1_.svg"
-    //         className="h-8"
-    //         alt="Flowbite Logo"
-    //       />
-    //       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-    //         Interbooks
-    //       </span>
-    //     </a>
-    //     <button
-    //       data-collapse-toggle="navbar-default"
-    //       type="button"
-    //       className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-    //       aria-controls="navbar-default"
-    //       aria-expanded="false"
-    //     >
-    //       <span className="sr-only">Open main menu</span>
-    //       <svg
-    //         className="w-5 h-5"
-    //         aria-hidden="true"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //         fill="none"
-    //         viewBox="0 0 17 14"
-    //       >
-    //         <path
-    //           stroke="currentColor"
-    //           strokeLinecap="round"
-    //           strokeLinejoin="round"
-    //           strokeWidth="2"
-    //           d="M1 1h15M1 7h15M1 13h15"
-    //         />
-    //       </svg>
-    //     </button>
-    //     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-    //       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-    //         <li>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-    //             aria-current="page"
-    //           >
-    //             Home
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    //           >
-    //             Perfil
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    //           >
-    //             Biblioteca
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    //           >
-    //             Deseados
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-    //           >
-    //             Intercambio
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
+    </nav>
   );
 }
+
+export default Navbar;
