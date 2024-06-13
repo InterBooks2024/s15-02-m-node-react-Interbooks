a5dcadacc4efec55dc205cb8cb2b771b4fa3a69b
Matias Saade <matiassaade@gmail.com>
2024-06-13 08:42:27 -0300

    fix: eliminados console.logs

--- front/src/views/home/Home.jsx
+++ front/src/views/home/Home.jsx
@@ -3,9 +3,7 @@ import { useBook } from '../../hooks/useBook'
 
 
 export function Home() {
-  const { userBooks, books } = useBook()
-
-  console.log(books)
+  const { books } = useBook()
 
   return (
     <main className='container p-2 md:p-10 h-full w-full mx-auto'>
